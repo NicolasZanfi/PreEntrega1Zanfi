ae7d2bdc706c43900c45070f29aafd42c7ca9e23
NicolasZanfi <115593664+NicolasZanfi@users.noreply.github.com>
2023-01-26 20:26:38 -0300

    Add files via upload

---
+++ PreEntrega1Zanfi/js/main.js
@@ -0,0 +1,21 @@
+
+
+// Servicios y precios
+
+const manicura = 5000;
+const pedicura = 6000;
+const color = 7500;
+
+let carrito = prompt("¿Qué servicio deseas agregar? (manicura, pedicura, color)");
+// hasta creo que voy bien y lo entiendo
+console.log("El valor es :$")
+
+
+
+/*let total = 0
+
+for (let i = 0; i< length; i++){
+
+console.log(i) = + carrito;}
+
+*/
